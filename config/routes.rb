98814610a5eff49e2665bbf9ff83e6d203ddb99f
lauren.kruczyk <lauren.kruczyk@gmail.com>
2014-04-17 11:27:16 -0400

    css practice

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :circle_navs
 
   resources :menu_effects
+    root "menu_effects#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
